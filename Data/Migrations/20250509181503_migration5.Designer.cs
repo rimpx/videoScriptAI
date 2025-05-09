491419272295a6671623ce2b23c2b753e8f352c6
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2025-05-09 22:01:01 +0200

    sistemazione di alcune problematiche

--- Data/Migrations/20250509181503_migration5.Designer.cs
+++ Data/Migrations/20250509181503_migration5.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using videoscriptAI.Data;
 
@@ -11,9 +12,10 @@ using videoscriptAI.Data;
 namespace videoscriptAI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250509181503_migration5")]
+    partial class migration5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
